2cf976d2dd4f3d94d18dadee21b696c41d90c95c
tengxing <tengxing7452@163.com>
2017-07-29 17:49:28 +0800

    添加登录

---
+++ src/main/java/cn/yjxxclub/bgApi/yjxx/controller/LoginController.java
@@ -0,0 +1,11 @@
+package cn.yjxxclub.bgApi.yjxx.controller;
+
+/**
+ * Author: Starry.Teng
+ * Email: tengxing7452@163.com
+ * Date: 17-7-25
+ * Time: 下午1:16
+ * Describe: 登录控制器
+ */
+public class LoginController {
+}
