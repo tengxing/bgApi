173812852a5057b252e11aaad7215d115ed34686
tengxing <tengxing7452@163.com>
2017-07-29 17:55:43 +0800

    问题模块

---
+++ src/main/java/cn/yjxxclub/bgApi/question/mapper/QuestionMapper.java
@@ -0,0 +1,16 @@
+package cn.yjxxclub.bgApi.question.mapper;
+
+import cn.yjxxclub.bgApi.common.mapper.BaseMapper;
+import cn.yjxxclub.bgApi.question.entity.Question;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Author: Starry.Teng
+ * Email: tengxing7452@163.com
+ * Date: 17-7-26
+ * Time: 下午11:30
+ * Describe: QuestionMapper
+ */
+@Mapper
+public interface QuestionMapper extends BaseMapper<Question> {
+}
---
+++ src/main/java/cn/yjxxclub/bgApi/question/service/QuestionService.java
@@ -0,0 +1,14 @@
+package cn.yjxxclub.bgApi.question.service;
+
+import cn.yjxxclub.bgApi.common.mapper.BaseMapper;
+import cn.yjxxclub.bgApi.question.entity.Question;
+
+/**
+ * Author: Starry.Teng
+ * Email: tengxing7452@163.com
+ * Date: 17-7-26
+ * Time: 下午11:25
+ * Describe: Question Service
+ */
+public interface QuestionService extends BaseMapper<Question> {
+}
