a73bf6925f7c6c43fba04c6c75fff40bc6713bc2
tengxing <tengxing7452@163.com>
2017-07-29 17:09:01 +0800

    问题模块后台api

--- src/main/java/cn/yjxxclub/bgApi/question/service/impl/QuestionServiceImpl.java
+++ src/main/java/cn/yjxxclub/bgApi/question/service/impl/QuestionServiceImpl.java
@@ -1,11 +1,20 @@
 package cn.yjxxclub.bgApi.question.service.impl;
 
+import cn.yjxxclub.bgApi.common.service.impl.BaseServiceImpl;
+import cn.yjxxclub.bgApi.question.entity.Question;
+import cn.yjxxclub.bgApi.question.mapper.QuestionMapper;
+import cn.yjxxclub.bgApi.question.service.QuestionService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Author: Starry.Teng
  * Email: tengxing7452@163.com
  * Date: 17-7-26
  * Time: 下午11:29
- * Describe:
+ * Describe:QuestionService实现类
  */
-public class QuestionServiceImpl {
+@Service("QuestionService")
+@Transactional
+public class QuestionServiceImpl extends BaseServiceImpl<QuestionMapper,Question> implements QuestionService{
 }
