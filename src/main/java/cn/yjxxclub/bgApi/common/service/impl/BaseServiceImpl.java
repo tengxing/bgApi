300ad964e405af7cf1edfe93db5ee03978aa37c9
tengxing <tengxing7452@163.com>
2017-07-29 17:55:02 +0800

    公共部分

---
+++ src/main/java/cn/yjxxclub/bgApi/common/service/impl/BaseServiceImpl.java
@@ -0,0 +1,57 @@
+package cn.yjxxclub.bgApi.common.service.impl;
+
+import cn.yjxxclub.bgApi.common.mapper.BaseMapper;
+import cn.yjxxclub.bgApi.common.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Author: Starry.Teng
+ * Email: tengxing7452@163.com
+ * Date: 17-7-26
+ * Time: 下午10:48
+ * Describe: BaseService实现类
+ */
+public class BaseServiceImpl<S extends BaseMapper,T> implements BaseService<T> {
+
+
+    @Autowired
+    BaseMapper<T> baseMapper;
+
+    @Override
+    public Integer add(T entity) {
+        return baseMapper.add(entity);
+    }
+
+    @Override
+    public Integer update(T entity) {
+        return baseMapper.update(entity);
+    }
+
+    @Override
+    public long countTotal() {
+        return baseMapper.countTotal();
+    }
+
+    @Override
+    public List listTotal() {
+        return baseMapper.listTotal();
+    }
+
+    @Override
+    public Integer deleteById(Integer id) {
+        return baseMapper.deleteById(id);
+    }
+
+    @Override
+    public Integer updateByMap(Map map) {
+        return baseMapper.updateByMap(map);
+    }
+
+    @Override
+    public List listByPage(Map map) {
+        return baseMapper.listByPage(map);
+    }
+}
