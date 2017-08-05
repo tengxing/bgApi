9d76b32863a474f449b567cf1692ee5c79a9d3e2
tengxing <tengxing7452@163.com>
2017-08-06 02:07:13 +0800

    修改

---
+++ src/test/java/cn/yjxxclub/bgApi/jedis/SpringJedisSentinelTest.java
@@ -0,0 +1,11 @@
+package cn.yjxxclub.bgApi.jedis;
+
+/**
+ * Author: Starry.Teng
+ * Email: tengxing7452@163.com
+ * Date: 17-8-6
+ * Time: 上午1:15
+ * Describe:
+ */
+public class SpringJedisSentinelTest {
+}
