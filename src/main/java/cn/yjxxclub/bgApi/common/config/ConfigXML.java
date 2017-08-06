87335d4548a7860ae6c7f0602ee48be8ebf43a28
tengxing <tengxing7452@163.com>
2017-08-06 15:13:46 +0800

    提交测试seninel

---
+++ src/main/java/cn/yjxxclub/bgApi/common/config/ConfigXML.java
@@ -0,0 +1,11 @@
+package cn.yjxxclub.bgApi.common.config;
+
+/**
+ * Author: Starry.Teng
+ * Email: tengxing7452@163.com
+ * Date: 17-8-6
+ * Time: 下午12:42
+ * Describe:
+ */
+public class ConfigXML {
+}
--- src/test/java/cn/yjxxclub/bgApi/jedis/SpringJedisSentinelTest.java
+++ src/test/java/cn/yjxxclub/bgApi/jedis/SpringJedisSentinelTest.java
@@ -1,11 +1,45 @@
 package cn.yjxxclub.bgApi.jedis;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
 /**
  * Author: Starry.Teng
  * Email: tengxing7452@163.com
  * Date: 17-8-6
  * Time: 上午1:15
- * Describe:
+ * Describe: spring+redis serntinel高可用集群测试
  */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"classpath*:redis.xml"})
 public class SpringJedisSentinelTest {
+    private static final Logger logger = LoggerFactory.getLogger(SpringJedisSentinelTest.class);
+
+    @Resource
+    RedisTemplate redisTemplate;
+
+    @Test
+    public void test() {
+        /*boolean result = (boolean) redisTemplate.execute((RedisCallback) connection -> {
+            connection.set("testKey".getBytes(), "test123".getBytes());
+            return true;
+        });*/
+        ValueOperations<String, String> valueOps = redisTemplate.opsForValue();
+        if (valueOps.get("testKey") == null) {
+            valueOps.set("testKey", "test123");
+            //logger.info("testKey:"+valueOps.get("testKey"));
+        } else {
+            logger.info("testKey:" + valueOps.get("testKey"));
+        }
+
+    }
+
 }
