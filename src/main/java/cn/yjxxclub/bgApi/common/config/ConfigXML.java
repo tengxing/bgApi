80feb0f3783eb04c503da051bc804c4ca6574202
tengxing <tengxing7452@163.com>
2017-08-07 21:38:23 +0800

    注解加载xml文件

--- src/main/java/cn/yjxxclub/bgApi/common/config/ConfigXML.java
+++ src/main/java/cn/yjxxclub/bgApi/common/config/ConfigXML.java
@@ -1,11 +1,15 @@
 package cn.yjxxclub.bgApi.common.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Author: Starry.Teng
  * Email: tengxing7452@163.com
  * Date: 17-8-6
  * Time: 下午12:42
- * Describe:
+ * Describe: 加载xml文件
  */
+@Configuration
+//@ImportResource(locations={"classpath*:sentinel.xml"})
 public class ConfigXML {
 }
